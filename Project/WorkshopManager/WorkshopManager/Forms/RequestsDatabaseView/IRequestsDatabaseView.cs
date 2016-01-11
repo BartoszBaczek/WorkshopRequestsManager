c8ec884f0866297f27d501f70e61167ac7741b4f
Bartek <bartosz.baczek@gmail.com>
2016-01-11 13:14:15 +0100

    Updated IRequestsDatabaseView

--- Project/WorkshopManager/WorkshopManager/Forms/RequestsDatabaseView/IRequestsDatabaseView.cs
+++ Project/WorkshopManager/WorkshopManager/Forms/RequestsDatabaseView/IRequestsDatabaseView.cs
@@ -1,6 +1,24 @@
-﻿namespace WorkshopManager.Forms.RequestsDatabaseView
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+
+namespace WorkshopManager.Forms.RequestsDatabaseView
 {
+    public enum RequestsCategory
+    {
+        Active,
+        Archivized
+    }
+
     public interface IRequestsDatabaseView
     {
+        RequestsDatabasePresenter Presenter { set; }
+
+        List<string> ActiveDataComboBox { get; set; }
+        int SelectedActiveData { get; set; }
+
+        DataTable DataGridViewSource { set; }
+        DataGridView DataGridView { get; }
+        DataGridViewRow SelectedRow { get; }
     }
 }

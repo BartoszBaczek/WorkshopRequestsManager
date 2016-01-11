1a9bf691cb3dd5b6dde16af51cf0d1677d1bc38f
Bartek <bartosz.baczek@gmail.com>
2016-01-11 13:27:33 +0100

    Implemented IRequestsDatabaseView in RequestsDatabaseForm

--- Project/WorkshopManager/WorkshopManager/Forms/RequestsDatabaseView/RequestsDatabaseView.cs
+++ Project/WorkshopManager/WorkshopManager/Forms/RequestsDatabaseView/RequestsDatabaseView.cs
@@ -1,12 +1,55 @@
-﻿using System.Windows.Forms;
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
 
-namespace WorkshopManager
+namespace WorkshopManager.Forms.RequestsDatabaseView
 {
-    public partial class Form1 : Form
+    public partial class RequestsDatabaseForm : Form, IRequestsDatabaseView
     {
-        public Form1()
+        public RequestsDatabasePresenter Presenter { private get; set; }
+
+        public RequestsDatabaseForm()
         {
             InitializeComponent();
+        }
+
+        public List<string> ActiveDataComboBox
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        public int SelectedActiveData
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        public DataTable DataGridViewSource
+        {
+            set { throw new System.NotImplementedException(); }
+        }
+
+        public DataGridView DataGridView
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public DataGridViewRow SelectedRow
+        {
+            get { throw new System.NotImplementedException(); }
         }
     }
 }

4089ab789a16d4d4fb6eb7d58401b09ce78d9e02
Bartek <bartosz.baczek@gmail.com>
2016-01-11 14:00:50 +0100

    Revert "Added form initialization method in Presenter"
    This reverts commit 938944c6b4823cac8ec02abb47794fefecf76d0a.

--- Project/WorkshopManager/WorkshopManager/Forms/RequestsDatabaseView/RequestsDatabasePresenter.cs
+++ Project/WorkshopManager/WorkshopManager/Forms/RequestsDatabaseView/RequestsDatabasePresenter.cs
@@ -1,27 +1,10 @@
-﻿using System;
-
-namespace WorkshopManager.Forms.RequestsDatabaseView
+﻿namespace WorkshopManager.Forms.RequestsDatabaseView
 {
     public class RequestsDatabasePresenter
     {
-        private IRequestsDatabaseView _view;
-
         public RequestsDatabasePresenter(IRequestsDatabaseView view)
         {
-            _view = view;
-            _view.Presenter = this;
-
-            Init();
-        }
-
-        public void Init()
-        {
-            SetActiveComboBoxDataSource();
-        }
-
-        private void SetActiveComboBoxDataSource()
-        {
-            _view.ActiveDataComboBox = Enum.GetValues(typeof (RequestsCategory));
+            
         }
     }
 }

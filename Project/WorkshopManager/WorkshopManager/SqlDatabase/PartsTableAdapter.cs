0310f8633810c11c7974703f648907ceeced2b30
McMorn <feliksik.pawel@gmail.com>
2016-01-13 00:14:43 +0100

    Create PartsTableAdapter

---
+++ Project/WorkshopManager/WorkshopManager/SqlDatabase/PartsTableAdapter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkshopManager.SqlDatabase
+{
+    using PartsTableAdapterExtension;
+
+    class PartsTableAdapter
+    {
+    }
+}
+
+namespace PartsTableAdapterExtension
+{
+    using WorkshopManager.SqlDatabase.MySql;
+
+    class Get
+    {
+        private static string _table = "Parts";
+        private static string _allColumns = "id, Name, Price";
+
+        public List<string>[] All()
+        {
+            return DBConnector.Select(
+                _allColumns,
+                false,
+                _table,
+                "");
+        }
+
+        public List<string>[] ByName(string name)
+        {
+            return DBConnector.Select(
+                _allColumns,
+                false,
+                _table,
+                string.Format("Name=\'{0}\'", name));
+        }
+
+        public List<string>[] ByPrice(float price)
+        {
+            return DBConnector.Select(
+                _allColumns,
+                false,
+                _table,
+                string.Format("Price=\'{0}\'", price));
+        }
+    }
+}

685bfff6d2dc0fd4ded41c58007bc106392304e0
Bartek <bartosz.baczek@gmail.com>
2016-01-12 21:07:49 +0100

    same changes as Part

---
+++ Project/WorkshopManager/WorkshopManager/Models/Request.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace WorkshopManager
+{
+    class Request
+    {
+        public int ID { get; private set; }     //ID tak samo jak w Part
+        public string Model { get; set; }
+        public string Owner { get; set; }
+        public string Description { get; set; }
+        public List<Part> ListOfParts { get; set; }
+
+        public Request(int id, string model, string owner, string description, List<Part> listOfParts)
+        {
+            ID = id;
+            Model = model;
+            Owner = owner;
+            Description = description;
+            ListOfParts = listOfParts;
+        }
+    }
+}

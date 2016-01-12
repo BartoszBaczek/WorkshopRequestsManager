b0e1a366277d5a5a1378c51d2d41de175af9fe6b
Bartek <bartosz.baczek@gmail.com>
2016-01-12 13:17:12 +0100

    Deleted not used 'usings'

--- Project/WorkshopManager/WorkshopManager/Forms/RequestsModifierView/ModifierPresenters/RequestAdderPresenter.cs
+++ Project/WorkshopManager/WorkshopManager/Forms/RequestsModifierView/ModifierPresenters/RequestAdderPresenter.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace WorkshopManager.Forms.RequestsModifierView
+﻿namespace WorkshopManager.Forms.RequestsModifierView
 {
     public class RequestAdderPresenter : RequestsModifierPresenter
     {

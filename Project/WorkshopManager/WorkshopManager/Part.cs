e436d5770d7eda31d6b42042c62e0d607ac90baa
avgorny <av.gorny@gmail.com>
2016-01-12 20:43:15 +0100

    KlasyersjaPierwsza
    Klasy w wersju rozwojowej. Dajcie jakieś wskazówki. Nie wiem czy to ma
    być coś takiego czy nie...

---
+++ Project/WorkshopManager/WorkshopManager/Part.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkshopManager
+{
+    class Part
+    {
+        private int id;
+        private string name;
+        private double prize;
+
+        Part()
+        {
+            name = "noname";
+            prize = '0';
+        }
+
+        Part(int _id, string _name, double _prize)
+        {
+            name = _name;
+            prize = _prize;
+
+        }
+    }
+}

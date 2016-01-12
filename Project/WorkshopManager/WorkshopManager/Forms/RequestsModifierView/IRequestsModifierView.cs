c6e3726e2eb384bb1bc515427172934bede0fc5d
Bartek <bartosz.baczek@gmail.com>
2016-01-12 12:15:21 +0100

    Updated Modifier View interface

--- Project/WorkshopManager/WorkshopManager/Forms/RequestsModifierView/IRequestsModifierView.cs
+++ Project/WorkshopManager/WorkshopManager/Forms/RequestsModifierView/IRequestsModifierView.cs
@@ -2,6 +2,8 @@
 {
     public interface IRequestsModifierView
     {
+        RequestsModifierPresenter Presenter { set; }
+
         string OwnerTextBox { get; set; }
         string CarModelTextBox { get; set; }
         string DescriptionTextBox { get; set; }

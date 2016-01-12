f8b6e96d1a6e1476c9e5b0df99671b936d9370e3
Bartek <bartosz.baczek@gmail.com>
2016-01-12 12:14:37 +0100

    Added interface for ModifierView

--- Project/WorkshopManager/WorkshopManager/Forms/RequestsModifierView/IRequestsModifierView.cs
+++ Project/WorkshopManager/WorkshopManager/Forms/RequestsModifierView/IRequestsModifierView.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WorkshopManager.Forms.RequestsModifierView
+﻿namespace WorkshopManager.Forms.RequestsModifierView
 {
     public interface IRequestsModifierView
     {
+        string OwnerTextBox { get; set; }
+        string CarModelTextBox { get; set; }
+        string DescriptionTextBox { get; set; }
     }
 }

a2670b88be9078fe64dbbf97ecd767f52d3b9a5c
Bartek <bartosz.baczek@gmail.com>
2016-01-12 12:41:03 +0100

    Implemented interface in Modifier view and updated constructor

--- Project/WorkshopManager/WorkshopManager/Forms/RequestsModifierView/RequestsModifierView.cs
+++ Project/WorkshopManager/WorkshopManager/Forms/RequestsModifierView/RequestsModifierView.cs
@@ -2,11 +2,32 @@
 
 namespace WorkshopManager.Forms.RequestsModifierView
 {
-    public partial class RequestsModifierView : Form
+    public partial class RequestsModifierView : Form, IRequestsModifierView
     {
+        public RequestsModifierPresenter Presenter { private get; set; }
+
         public RequestsModifierView()
         {
             InitializeComponent();
+            new RequestsModifierPresenter(this);
+        }
+
+        public string OwnerTextBox
+        {
+            get { return ownerTextBox.Text; }
+            set { ownerTextBox.Text = value; }
+        }
+
+        public string CarModelTextBox
+        {
+            get { return carModelTextBox.Text; }
+            set { carModelTextBox.Text = value; }
+        }
+
+        public string DescriptionTextBox
+        {
+            get { return descriptionTextBox.Text; }
+            set { descriptionTextBox.Text = value; }
         }
     }
 }

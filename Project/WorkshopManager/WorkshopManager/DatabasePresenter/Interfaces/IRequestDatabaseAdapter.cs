d52738d24558471e4451e6c93ed47e836f605288
Krystian Klein <krystian.klein.94@gmail.com>
2016-01-14 21:47:39 +0100

    Stworzenie podstawy
    Dodałem podstawy mojej działki: interface, i głowna klas.

---
+++ Project/WorkshopManager/WorkshopManager/DatabasePresenter/Interfaces/IRequestDatabaseAdapter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkshopManager.DatabasePresenter
+{
+    interface IRequestDatabaseAdapter
+    {
+        List<Part> GetByName();
+        List<Part> GetByPrize();
+        Part GetById();
+        void DeleteById(int id);
+        void UpdateById(int id); 
+    }
+}

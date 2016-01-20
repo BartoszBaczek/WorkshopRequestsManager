ad75f6a377731ba9896af7051b7a8265eec8b593
Bartek <bartosz.baczek@gmail.com>
2016-01-20 22:53:41 +0100

    and implemented them again
    also deleted old comment

--- Project/WorkshopManager/WorkshopManager/Models/Request.cs
+++ Project/WorkshopManager/WorkshopManager/Models/Request.cs
@@ -1,23 +1,27 @@
 ﻿using System.Collections.Generic;
+using WorkshopManager.Models.IRequest;
 
 namespace WorkshopManager
 {
-    public class Request
+    public class Request : IRequestWithIdAcces
     {
-        public int ID { get; private set; }     //ID tak samo jak w Part
+        public int ID { get; private set; }
         public string Model { get; set; }
         public string Owner { get; set; }
         public string Description { get; set; }
         public List<Part> ListOfParts { get; set; }
 
-        public Request(int id, string model, string owner, string description, List<Part> listOfParts)
+        public Request(string model, string owner, string description, List<Part> listOfParts)
         {
-            ID = id;
             Model = model;
             Owner = owner;
             Description = description;
             ListOfParts = listOfParts;
         }
 
+        public void SetId(int id)
+        {
+            ID = id;
+        }
     }
 }

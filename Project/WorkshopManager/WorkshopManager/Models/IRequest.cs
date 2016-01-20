8c50d314c5a2d1ed8548b1728da51523149b1773
Bartek <bartosz.baczek@gmail.com>
2016-01-20 22:05:37 +0100

    Added IRequest Interface

---
+++ Project/WorkshopManager/WorkshopManager/Models/IRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace WorkshopManager.Models
+{
+    public interface IRequest
+    {
+        int ID { get; }
+        string Model { get; set; }
+        string Owner { get; set; }
+        string Description { get; set; }
+        List<Part> ListOfParts { get; set; } 
+    }
+}

7bdfb6b03ffb1e0c67b701ba2c503db2bae2447e
Bartek <bartosz.baczek@gmail.com>
2016-01-24 15:40:19 +0100

    added new tools for request view

---
+++ Project/WorkshopManager/WorkshopManager/Forms/ToolsForRequestVIew/IToolsForRequestView.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace WorkshopManager.Forms.ToolsForRequestView
+{
+    public interface IToolsForRequestView
+    {
+        ToolsForRequestPresenter Presenter { set; }
+        List<object> AllToolsListBox { get; set; }
+        List<object> ToolsForRequestListBox { get; set; 
+    }
+}
---
+++ Project/WorkshopManager/WorkshopManager/Forms/ToolsForRequestVIew/ToolsForRequestPresenter.cs
@@ -0,0 +1,20 @@
+﻿namespace WorkshopManager.Forms.ToolsForRequestView
+{
+    public class ToolsForRequestPresenter
+    {
+        private IToolsForRequestView _view;
+
+        public ToolsForRequestPresenter(IToolsForRequestView view)
+        {
+            _view = view;
+            _view.Presenter = this;
+
+            Init();
+        }
+
+        private void Init()
+        {
+            
+        }
+    }
+}
